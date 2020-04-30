dd52dd675fd115968241457ee307991e9507ea35
Xavier Sanchez <xavier.sanchez@wallapop.com>
2020-04-30 13:27:24 +0200

    initial code. Moves and turns

---
+++ yatzee/src/main/java/com/wallapop/yatzee/MarsRover.kt
@@ -0,0 +1,53 @@
+package com.wallapop.yatzee
+
+class MarsRover {
+
+    private var y = 0
+    private var x = 0
+
+    private var heading = "N"
+
+    fun move(input: String): String {
+        input.chars().forEach { c ->
+            if (c == 'L'.toInt() && heading == "N")
+                heading = "W"
+            else if (c == 'L'.toInt() && heading == "E")
+                heading = "N"
+            else if (c == 'L'.toInt() && heading == "S")
+                heading = "E"
+            else if (c == 'L'.toInt() && heading == "W")
+                heading = "S"
+            else if (c == 'R'.toInt() && heading == "N")
+                heading = "E"
+            else if (c == 'R'.toInt() && heading == "E")
+                heading = "S"
+            else if (c == 'R'.toInt() && heading == "S")
+                heading = "W"
+            else if (c == 'R'.toInt() && heading == "W")
+                heading = "N"
+            else y++
+        }
+        return "$x $y $heading"
+    }
+
+    fun execute(input: String): String {
+        val (map, position, movements) = input.lines()
+        configureMap(map)
+        definePosition(position)
+        return move(movements)
+    }
+
+    private fun definePosition(position: String) {
+        val (x, y, heading) = position.split(' ')
+        this.x = x.toInt();
+        this.y = y.toInt();
+        this.heading = heading;
+
+    }
+
+
+    fun configureMap(input: String) {
+
+    }
+
+}
---
+++ yatzee/src/test/java/com/wallapop/yatzee/MarsRoverTest.kt
@@ -0,0 +1,107 @@
+package com.wallapop.yatzee
+
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+
+class MarsRoverTest {
+    @Test
+    fun `Void input does not make rover move`() {
+        val input = "5 5\n" +
+                "0 0 N\n";
+        val rover = MarsRover()
+        val result = rover.execute(input)
+        assertEquals(result, "0 0 N")
+    }
+
+    @Test
+    fun `move command makes rover goes north`() {
+        val input = "5 5\n0 0 N\nM"
+        val rover = MarsRover()
+        val result = rover.execute(input)
+        assertEquals(result, "0 1 N")
+    }
+
+    @Test
+    fun `L command makes rover turn west when facing north`() {
+        val input = "5 5\n" +
+                "0 0 N\nL"
+        val rover = MarsRover()
+        val result = rover.execute(input)
+        assertEquals(result, "0 0 W")
+    }
+
+    @Test
+    fun `LL command makes rover turn south when facing north`() {
+        val input = "5 5\n" +
+                "0 0 N\nLL"
+        val rover = MarsRover()
+        val result = rover.execute(input)
+        assertEquals(result, "0 0 S")
+    }
+
+    @Test
+    fun `LLL command makes rover turn east when facing north`() {
+        val input = "5 5\n" +
+                "0 0 N\nLLL"
+        val rover = MarsRover()
+        val result = rover.execute(input)
+        assertEquals(result, "0 0 E")
+    }
+
+    @Test
+    fun `LLLL command makes rover turn north when facing north`() {
+        val input = "5 5\n" +
+                "0 0 N\nLLLL"
+        val rover = MarsRover()
+        val result = rover.execute(input)
+        assertEquals(result, "0 0 N")
+    }
+
+    @Test
+    fun `RRRRR command makes rover turn east when facing north`() {
+        val input = "5 5\n" +
+                "0 0 N\nRRRRR"
+        val rover = MarsRover()
+        val result = rover.execute(input)
+        assertEquals(result, "0 0 E")
+    }
+
+    @Test
+    fun `define initial postion`() {
+        val input = "5 5\n" +
+                "1 1 E\n"
+        val rover = MarsRover()
+        val result = rover.execute(input)
+        assertEquals(result, "1 1 E")
+    }
+
+//    @Test
+//    fun `LM command makes rover move to east when facing north`() {
+//        val input = "LM"
+//        val rover = MarsRover()
+//        val result = rover.execute(input)
+//        assertEquals(result, "1 0 E")
+//    }
+
+    @Test
+    fun `2 consecutive move commands make rover goes north 2 positions`() {
+        val input = "5 5\n" +
+                "0 0 N\nMM"
+        val rover = MarsRover()
+        val result = rover.execute(input)
+
+        assertEquals(result, "0 2 N")
+    }
+
+    @Test
+    fun `5 consecutive move commands make rover goes north 5 positions`() {
+        val input = "5 5\n" +
+                "0 0 N\nMMMMM"
+        val rover = MarsRover()
+        val result = rover.execute(input)
+
+        assertEquals(result, "0 5 N")
+    }
+
+
+}
